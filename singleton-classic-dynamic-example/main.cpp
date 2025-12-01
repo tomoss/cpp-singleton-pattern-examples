3b66de6aa3562fbb7c61421bf076d1ad12992055
Ionut Tomos <ionut.tomoss@gmail.com>
2025-12-02 00:46:04 +0200

    Added another example

---
+++ singleton-classic-dynamic-example/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "singleton.h"
+
+/*
+* Example with static pointer member variable
+* Dynamic memory allocation
+* Lazy initialization
+* Singleton is created only after first call of getInstance() and destroyed by calling delInstance()
+* Thread-safety not guaranteed
+*/
+
+int main()
+{
+    std::cout << "--- main start ---" << std::endl;
+
+    Singleton::getInstance().func();
+    Singleton::getInstance().func();
+    Singleton::delInstance();
+
+    std::cout << "--- main end ---" << std::endl;
+}
---
+++ singleton-classic-dynamic-example/src/singleton.cpp
@@ -0,0 +1,30 @@
+#include "singleton.h"
+#include <iostream>
+
+Singleton* Singleton::instance = nullptr;
+
+Singleton& Singleton::getInstance() {
+    if (!instance) {
+        instance = new Singleton(); 
+    }
+    return *instance;
+}
+
+void Singleton::delInstance() {
+    if (instance) {        
+        delete instance;
+        instance = nullptr;
+    }
+}
+
+Singleton::Singleton() {
+    std::cout << "Singleton created." << std::endl;
+}
+
+void Singleton::func() {
+    std::cout << "Doing something..." << std::endl;
+}
+
+Singleton::~Singleton() {
+    std::cout << "Singleton destroyed." << std::endl;
+}
